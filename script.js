85402026441c3ff7b0ca0468ee1472e41342df15
daisywebde <154891967+daisywebde@users.noreply.github.com>
2024-07-20 19:58:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+ document.querySelector('.cross').style.display = 'none';
+ document.querySelector('.hamburger').addEventListener("click", ()=> { document.querySelector('.side-bar').classList.toggle('side-barGo');
+    if( document.querySelector('.side-bar').classList.contains('side-barGo')){
+        document.querySelector('.ham').style.display = 'inline';
+        document.querySelector('.cross').style.display = 'none';
+    }
+    else {
+        document.querySelector('.ham').style.display = 'none';
+        setTimeout(()=>{document.querySelector('.cross').style.display = 'inline';},350);
+        
+    }
+ });
+ 
